5e77f595db9e13299d94ed40e9b7f3ce83826279
federer250 <80335509+federer250@users.noreply.github.com>
2022-09-09 10:05:49 -0300

    cambios en el front

--- src/app/service/educacion.service.ts
+++ src/app/service/educacion.service.ts
@@ -7,7 +7,7 @@ import { Educacion } from '../model/educacion';
   providedIn: 'root'
 })
 export class EducacionService {
-  URL = 'htttp://localhost:8080/educacion/';
+  URL = 'http://localhost:8080/educacion/';
 
 
 
